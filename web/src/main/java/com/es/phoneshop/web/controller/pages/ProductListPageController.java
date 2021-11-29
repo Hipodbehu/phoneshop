ea879f2a5b3c67be7879329de10c00096c9576df
Hipodbehu <hipodbehu@gmail.com>
2021-11-29 21:17:45 +0300

    Renamed methods

--- web/src/main/java/com/es/phoneshop/web/controller/pages/ProductListPageController.java
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/ProductListPageController.java
@@ -3,6 +3,7 @@ package com.es.phoneshop.web.controller.pages;
 import com.es.core.model.product.ProductDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,12 +16,12 @@ public class ProductListPageController {
   public static final String DEFAULT_ORDER = "id";
   public static final String DEFAULT_ORDER_DIRECTION = "desc";
   public static final String DEFAULT_QUERY = "";
+  public static final String DEFAULT_PAGE_NUMBER = "1";
   public static final String PHONES_ATTRIBUTE = "phones";
   public static final String PHONES_PAGE_NUMBER_ATTRIBUTE = "phonesPageNumber";
-  public static final String PRODUCT_LIST_PAGE = "productList";
-  public static final String DEFAULT_PAGE_NUMBER = "1";
-  public static final int PRODUCT_LIMIT = 10;
   public static final String PAGE_NUMBER_ATTRIBUTE = "pageNumber";
+  public static final String PRODUCT_LIST_PAGE = "productList";
+  public static final int PRODUCT_LIMIT = 10;
 
   @Resource
   private ProductDao productDao;
@@ -31,12 +32,12 @@ public class ProductListPageController {
                                 @RequestParam(required = false, defaultValue = DEFAULT_ORDER) String order,
                                 @RequestParam(required = false, defaultValue = DEFAULT_ORDER_DIRECTION) String orderDirection,
                                 @RequestParam(required = false, defaultValue = DEFAULT_PAGE_NUMBER) String pageNumber) {
-    query = checkQuery(query);
-    order = checkOrder(order);
-    orderDirection = checkOrderDirection(orderDirection);
+    query = getQuery(query);
+    order = getOrder(order);
+    orderDirection = getOrderDirection(orderDirection);
     int countOfPhones = productDao.getCount(query);
     int countOfPages = countOfPhones / PRODUCT_LIMIT;
-    int pageNum = checkPageNumber(pageNumber, countOfPages);
+    int pageNum = getPageNumber(pageNumber, countOfPages);
     model.addAttribute(PHONES_PAGE_NUMBER_ATTRIBUTE, countOfPages);
     model.addAttribute(PAGE_NUMBER_ATTRIBUTE, pageNum);
     model.addAttribute(PHONES_ATTRIBUTE,
@@ -44,23 +45,23 @@ public class ProductListPageController {
     return PRODUCT_LIST_PAGE;
   }
 
-  private String checkQuery(String query) {
-    if (query == null) {
+  private String getQuery(String query) {
+    if (StringUtils.isEmpty(query)) {
       query = DEFAULT_QUERY;
     }
     return query;
   }
 
-  private String checkOrder(String order) {
-    if (order == null || order.isEmpty()) {
+  private String getOrder(String order) {
+    if (StringUtils.isEmpty(order)) {
       order = DEFAULT_ORDER;
     }
     return order;
   }
 
-  private Integer checkPageNumber(String pageNumber, int countOfPages) {
+  private Integer getPageNumber(String pageNumber, int countOfPages) {
     int result = 1;
-    if (pageNumber != null && !pageNumber.isEmpty()) {
+    if (StringUtils.isEmpty(pageNumber)) {
       try {
         result = Integer.parseInt(pageNumber);
         if (result > countOfPages) {
@@ -75,8 +76,8 @@ public class ProductListPageController {
     return result;
   }
 
-  private String checkOrderDirection(String orderDirection) {
-    if (orderDirection == null || orderDirection.isEmpty()) {
+  private String getOrderDirection(String orderDirection) {
+    if (StringUtils.isEmpty(orderDirection)) {
       orderDirection = DEFAULT_ORDER_DIRECTION;
     }
     return orderDirection;
